a46da68b42fb92e87b98ab5b3a867194096465d0
almir4409 <dautialmir4.com>
2024-05-20 14:56:29 +0200

    login and register design

---
+++ example-app/resources/views/app/signUp.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.main')
+
+@section('title', 'Sign Up')
+
+@section('content')
+<section class="bg-gray-900">
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900">
+            <img src="./brainster-learn-logo 2.png" alt="logo">  
+        </a>
+        <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 class="text-xl font-bold leading-tight tracking-tight text-[#8448e5] md:text-2xl">
+                    Create your account
+                </h1>
+                <form class="space-y-4 md:space-y-6" action="#">
+                    <div>
+                        <label for="name" class="block mb-2 text-sm font-medium text-[#8448e5]">Your name</label>
+                        <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5" placeholder="John Doe" required="">
+                    </div>
+                    <div>
+                        <label for="email" class="block mb-2 text-sm font-medium text-[#8448e5]">Your email</label>
+                        <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5" placeholder="name@company.com" required="">
+                    </div>
+                    <div>
+                        <label for="password" class="block mb-2 text-sm font-medium text-[#8448e5]">Password</label>
+                        <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5" required="">
+                    </div>
+                    <div class="flex items-center justify-between">
+                        <div class="flex items-start">
+                            <div class="flex items-center h-5">
+                                <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50">
+                            </div>
+                            <div class="ml-3 text-sm">
+                                <label for="remember" class="text-gray-500">Remember me</label>
+                            </div>
+                        </div>
+                    </div>
+                    <button type="submit" class="w-full p-2 rounded-xl text-white bg-[#8448e5] hover:bg-[#101010] hover:border-[1px] hover:border-[#8448e5]">Sign up</button>
+                    <p class="text-sm font-light text-gray-500">
+                        Already have an account? <a href="{{ route('login') }}" class="font-medium text-[#f54646] hover:underline">Log in</a>
+                    </p>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+@endsection
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [EventController::class, "index"])->name('view-events');
@@ -8,3 +9,5 @@ Route::get('/create-event', [EventController::class, "create"])->name('create.ev
 Route::post('/store-event', [EventController::class, 'store'])->name('store.event');
 
 
+Route::get('/login', [UserController::class, "login"])->name('login');
+Route::get('/signup', [UserController::class, "signUp"])->name('signup');
