28eb857dea418d4d3480b53d02fbac285294cbab
Daniel Dzonov <dzonovdaniel@gmail.com>
2024-06-10 16:21:07 +0200

    fix for responsive design

--- example-app/resources/views/app/index.blade.php
+++ example-app/resources/views/app/index.blade.php
@@ -85,7 +85,7 @@
 
 
         <div class=" gap-5 justify-between mt-16 mb-16 w-full text-lg leading-8 text-white max-md:flex-wrap max-md:mt-10 max-md:max-w-full">
-            <div class="!flex  items-center gap-5 !justify-between flex-col w-1/3 md:w-full !m-auto font-medium max-md:flex-wrap !md:flex-row">
+            <div class="!flex  items-center gap-5 !justify-between flex-column w-1/3 md:w-full !m-auto font-medium max-md:flex-wrap md:flex-row">
 
                 <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white !bg-[#8448E5] hover:opacity focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" type="button">All Events <svg class="ml-2" xmlns="http://www.w3.org/2000/svg" height="20" width="15" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.-->
                         <path fill="#ffffff" d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z" />
